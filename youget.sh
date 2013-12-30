df12eeee2b475ce08c2b18fac4443682d3e301c9
alycolas <alycolas@gmail.com>
2013-12-30 22:03:10 +0800

    change something by useing checkout b9c08f3 youget.sh

--- youget.sh
+++ youget.sh
@@ -3,12 +3,12 @@
 
 [[ "$2" = "-p" ]] && export http_proxy="http://127.0.0.1:8087"
 
-you-get -u $1 |
-grep "http" |
-sed -e "s/[][' )(]//g" -e "s/,/\n/g" |
+you-get -u $1 | 
+grep "http" | 
+sed -e "s/[][' )(]//g" -e "s/,/\n/g" | 
 while read url
 do 
-	wget --user-agent="Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.0.6) Gecko/20060728 Firefox/1.5" $url -O - 2>/dev/pts/0 |
-	mplayer -fs - |
+	wget --user-agent="Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.0.6) Gecko/20060728 Firefox/1.5" $url -O - 2>/dev/pts/0 | 
+	mplayer -fs - | 
 	grep Quit && exit 2
 done
